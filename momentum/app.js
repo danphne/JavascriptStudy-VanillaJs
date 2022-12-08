8344c42704249dffcefb2d5e5cab4b74f590d79e
danphne <dancerelrin@gmail.com>
2022-12-08 20:20:53 +0900

    object & function

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const player = {
+  name : 'daphne',
+  sayHello : function(personName){
+    console.log("hello! " + personName);
+  }
+}
+console.log(player.name);
+player.sayHello("lynn");
