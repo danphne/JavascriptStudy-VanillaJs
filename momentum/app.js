2f1c6df8c3ce0d6487585e7b6dc31d4c27cdde94
danphne <dancerelrin@gmail.com>
2022-12-09 21:01:56 +0900

    if && conditionals

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,15 @@
-const player = {
-  name : 'daphne',
-  sayHello : function(personName){
-    console.log("hello! " + personName);
-  }
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0){
+  console.log('Please write a real positive number');
+}else if(age < 18){
+  console.log('You are too young');
+}else if(age >= 18 && age <= 50){
+  console.log('You can drink');
+}else if(age > 50 && age <= 80){
+  console.log('You should exercise');
+}else if(age === 100){
+  console.log('WoW');
+}else if(age > 80){
+  console.log('You can do whatever you want');
 }
-console.log(player.name);
-player.sayHello("lynn");
