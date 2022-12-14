ec8b0eaa60bb411a22ae05d83d10a9e57ddab737
danphne <dancerelrin@gmail.com>
2022-12-14 20:40:00 +0900

    javascript Login

--- momentum/app.js
+++ momentum/app.js
@@ -1,14 +1,28 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick(){
-  const clickedClass = "clicked";
-  /*if(h1.classList.contains(clickedClass)){
-    h1.classList.remove(clickedClass);
-  }else{
-    h1.classList.add(clickedClass);
-  }*/
-  h1.classList.toggle(clickedClass);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+  event.preventDefault();
+  const username = loginInput.value;  
+  localStorage.setItem(USERNAME_KEY,username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-h1.addEventListener("click", handleTitleClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+  paintGreetings(savedUsername);
+}
